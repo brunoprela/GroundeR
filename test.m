f45ee3cc948c20836bb5629ffea9de972ec395e6
syq2012 <syq2012f@uw.edu>
2018-04-26 12:46:27 -0400

    add function for getting boxes and word sequence for each phrase

--- test.m
+++ test.m
@@ -5,6 +5,7 @@ Sen = getSentenceData(fn2);
 queryList = {};
 
 %get list of all query: get all phrases listed in flicker
+%this has a lot of repetetion.
 for i = 1:numel(Sen)
         temp = Sen(i).phraseID;
         for j = 1:numel(temp)
@@ -15,14 +16,25 @@ end
 disp(queryList)
 
 %find mapping for all phrases 
+%all this is not stored
 for i = 1:numel(Ann.id)
     %getBox
-    box = 
-    %getwordID sequences for this phrase
-    
-    %compute all positive prosoal
-    
-    %get best proposal
-    
-   
+    boxindex = Ann.idToLabel(i);
+    index = 0;
+    if length(boxindex) > 1
+        index = boxindex(1);
+    else 
+        index = boxindex;
+    end
+    box = Ann.labels(index);
+    %getwordID sequences for this phrase (now those are just words)
+    wordID = [];
+    for j = 1:numel(Sen)
+        temp = Sen(j).phraseID;
+        for k = 1:numel(temp)
+            if temp(k) == Ann.id(i)
+                wordID = Sen(j).phrases(k);
+            end
+        end
+    end
 end
