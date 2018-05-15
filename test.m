c476535a85543d91e38465826bc05afc065183f8
syq2012 <syq2012f@uw.edu>
2018-05-15 02:04:05 -0400

    modified:   test.m 	modified:   getPosp.m

--- test.m
+++ test.m
@@ -1,21 +1,31 @@
-fn = '';
-if ~exist(fn,'file')
-        error('file not found');
-end
-fid = fopen(fn, 'rt');
-while true
-    curline = fgetl(fid);
-    if ~ischar(curline)
-        break;
-    end
+% fn = '';
+% if ~exist(fn,'file')
+%         error('file not found');
+% end
+% fid = fopen(fn, 'rt');
+% while true
+%     curline = fgetl(fid);
+%     if ~ischar(curline)
+%         break;
+%     end
+%     fn1 = strcat('./Flickr30kEntities/Annotations/',curline, '.xml') ;
+%     fn2 = strcat('./Flickr30kEntities/Sentences/',curline, '.txt') ;
+%     fn3 = strcat('./flickr30k_img_bbx_ss/', curline, '.mat');
+
+files = dir('./Flickr30kEntities/Annotations/*.xml');
+for f = files'
+    curline = f.name(1:end-4);
+    disp(curline);
     fn1 = strcat('./Flickr30kEntities/Annotations/',curline, '.xml') ;
     fn2 = strcat('./Flickr30kEntities/Sentences/',curline, '.txt') ;
     fn3 = strcat('./flickr30k_img_bbx_ss/', curline, '.mat');
+    
+    
 
-
-%fn1 = './Flickr30kEntities/Annotations/6609688031.xml' ;
-%fn2 = './Flickr30kEntities/Sentences/6609688031.txt';
-%fn3 = './flickr30k_img_bbx_ss/6609688031.mat';
+%     fn1 = './Flickr30kEntities/Annotations/6609688031.xml' ;
+%     fn2 = './Flickr30kEntities/Sentences/6609688031.txt';
+%     fn3 = './flickr30k_img_bbx_ss/6609688031.mat';
+%     curline = '1001773457;';
 
     Ann = getAnnotations(fn1);
     Sen = getSentenceData(fn2);
@@ -40,6 +50,25 @@ while true
     %find mapping for all phrases 
     %all this is not stored
     for i = 1:numel(Ann.id)
+        pcount = 0;
+        wordID = [];
+        for j = 1:numel(Sen)
+            temp = Sen(j).phraseID;
+            for k = 1:numel(temp)
+                disp(curline)
+                disp(size(temp{k}))
+                disp(size(Ann.id{i}))
+                disp(temp{k})
+                disp(Ann.id{i})
+                if eq(str2double(temp{k}), str2double(Ann.id{i})) 
+                    pcount = pcount + 1;
+                    if isempty(wordID)
+                        wordID = Sen(j).phrases(k);
+                    end
+                end
+            end
+        end
+
         %getBox if multiple chose first one
 
         %disp(Ann.id{i})
@@ -49,62 +78,68 @@ while true
             for j = 1:m
                 curLabel = Ann.labels(boxindex{1}(j, 1));
                 box = curLabel.boxes;
-
+                %nobox = curLabel.nobox;
                 %[a, b] = size(box)
-                if curLabel.nobox == 0
+                %disp(isempty(nobox));
+                if isempty(curLabel.nobox)
                     [prop_list, best_box] = getPosp(proposals_matrix, box);
-                else
+                elseif curLabel.nobox == 1
                     %for when there's no box
                     box = [0, 0, 0, 0];
-                    prop_list = [];
+                    prop_list = [-1];
                     best_box = [-1];
                 end
-                queryList = [queryList, Ann.id{i}];
-                boxList = [boxList, box];
-                propList = [propList, prop_list]
-                propidList = [propidList, best_box]
+                for t = 1:pcount
+                    queryList = [queryList, Ann.id{i}];
+                    boxList = [boxList, box];
+                    propList = [propList, prop_list];
+                    propidList = [propidList, best_box];
+                    wordList = [wordList, wordID]
                 %disp(prop_list)
+                end
             end
         else 
 
-            curLabel = Ann.labels(boxindex{1}(j, 1));
+            curLabel = Ann.labels(boxindex{1}(1, 1));
             box = curLabel.boxes;
 
             %box = Ann.labels(boxindex{1}).boxes;
 
             %[a, b] = size(box)
-            if curLabel.nobox == 0
+            if isempty(curLabel.nobox)
 
                 [prop_list, best_box] = getPosp(proposals_matrix, box);
-            else
-
+            elseif curLabel.nobox == 1
                 box = [0, 0, 0, 0];
-                prop_list = [];
+                prop_list = [-1];
                 best_box = [-1];
             end
-            queryList = [queryList, Ann.id{i}];
-            boxList = [boxList, box];    
-            %[prop_list, best_box] = getPosp(proposals_matrix, box);
-            propList = [propList, prop_list]
-            propidList = [propidList, best_box]
+            for t = 1:pcount
+                queryList = [queryList, Ann.id{i}];
+                boxList = [boxList, box];    
+                %[prop_list, best_box] = getPosp(proposals_matrix, box);
+                propList = [propList, prop_list]
+                propidList = [propidList, best_box]
+                wordList = [wordList, wordID]
+            end
 
         end
         %getwordID sequences for this phrase (now those are just words)
-        wordID = [];
-        for j = 1:numel(Sen)
-            temp = Sen(j).phraseID;
-            for k = 1:numel(temp)
-                if and((temp{k} == Ann.id{i}),(isempty(wordID))) 
-                    wordID = Sen(j).phrases(k);
-                end
-            end
-        end
-
-        for j = 1:numel(boxindex)
-            wordList = [wordList, wordID]
-        end
+%         wordID = [];
+%         for j = 1:numel(Sen)
+%             temp = Sen(j).phraseID;
+%             for k = 1:numel(temp)
+%                 if and((temp{k} == Ann.id{i}),(isempty(wordID))) 
+%                     wordID = Sen(j).phrases(k);
+%                 end
+%             end
+%         end
+% 
+%         for j = 1:m
+%             wordList = [wordList, wordID]
+%         end
     end
-    filename = strcat('./annotation/', curline, '.mat');
-    save(filename, queryList, boxList, wordList, propList, propidList);
+    temp = strcat(curline, '.mat');
+    save(temp, 'queryList', 'boxList', 'wordList', 'propList', 'propidList');
 end
  
